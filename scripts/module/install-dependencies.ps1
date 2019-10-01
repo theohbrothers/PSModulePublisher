81a82399bafd43d6d99ff8754a0dfb21aa5df6b8
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-02 03:58:35 +0800

    install-dependencies.ps1: Standardize PowerShellGet module name in comments

--- scripts/module/install-dependencies.ps1
+++ scripts/module/install-dependencies.ps1
@@ -6,7 +6,7 @@ try {
     "Retrieving info on PSGallery repository" | Write-Host
     Get-PSRepository -Name 'PSGallery' | Format-List -Property * | Out-String | Write-Verbose
 
-    # Install PowershellGet module of the specified version
+    # Install PowerShellGet module of the specified version
     "Installing PowerShellGet" | Write-Host
     $powershellGetRequiredVersion = '2.1.2'
     $powershellGetInstalledVersions = (Get-Module 'PowerShellGet' -ListAvailable).Version | % { $_.ToString() }
@@ -14,7 +14,7 @@ try {
         Install-Module -Name 'PowershellGet' -Repository 'PSGallery' -RequiredVersion $powershellGetRequiredVersion -Scope CurrentUser -Force
     }
 
-    # Import and get info on PowershellGet
+    # Import and get info on PowerShellGet
     "Importing PowerShellGet" | Write-Host
     Import-Module -Name 'PowerShellGet' -RequiredVersion $powershellGetRequiredVersion -Force
     Get-Module -Name 'PowerShellGet' -ListAvailable | Out-String | Write-Verbose
