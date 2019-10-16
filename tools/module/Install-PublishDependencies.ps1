aba281d5cfd1aab59353a25b7efd8aa111532c4f
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-16 20:26:18 +0800

    Improve Install-PublishDependencies.ps1

--- tools/module/Install-PublishDependencies.ps1
+++ tools/module/Install-PublishDependencies.ps1
@@ -1,17 +1,17 @@
 [CmdletBinding()]
 param()
 
-# Issue with PowerShell 5.0 not having NuGet.exe despite installing the package provider. The binary is necessary for Publish-Module to work
 "PowerShell version: $($PSVersionTable.PSVersion.ToString())" | Write-Host
-"Installing NuGet binary if needed" | Write-Host
+
+# Install the NuGet binary For PowerShell versions below 5.1 for Publish-Module to work correctly
+"Installing NuGet.exe" | Write-Host
 if ($PSVersionTable.PSVersion -lt [version]'5.1') {
-    "Checking for NuGet binary"
     $nugetBinPath = "$env:LOCALAPPDATA\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
-    Get-ChildItem -Path "$env:LOCALAPPDATA\Microsoft\Windows\PowerShell" -Recurse
+    "Checking for NuGet.exe" | Write-Host
     if (!(Test-Path -Path $nugetBinPath -PathType Leaf)) {
-        "Downloading NuGet binary" | Write-Host
-        New-Item -Path (Split-Path -Path $nugetBinPath -Parent) -ItemType Directory -Force
-        Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile $nugetBinPath
+        New-Item -Path (Split-Path -Path $nugetBinPath -Parent) -ItemType Directory -Force > $null
+        "Downloading NuGet.exe" | Write-Host
+        Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile $nugetBinPath
     }
     Get-ChildItem -Path (Split-Path -Path $nugetBinPath -Parent)
     & $nugetBinPath
