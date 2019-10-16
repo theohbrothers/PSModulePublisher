e76a47a813f8bbee82a4bf36f0506974a7eb76d9
Joel Timothy Oh <joeltimothyoh@live.com>
2019-10-17 01:49:30 +0800

    Install-PublishDependencies.ps1: Install nuget binary with a lowercase name. Tweak console output

--- tools/module/Install-PublishDependencies.ps1
+++ tools/module/Install-PublishDependencies.ps1
@@ -3,18 +3,18 @@ param()
 
 "PowerShell version: $($PSVersionTable.PSVersion.ToString())" | Write-Host
 
-# Install the NuGet binary For PowerShell versions below 5.1 for Publish-Module to work correctly
-"Installing NuGet.exe" | Write-Host
+# Install the NuGet binary for PowerShell versions below 5.1 for Publish-Module to work correctly
+"Verifying if NuGet should be installed" | Write-Host
 if ($PSVersionTable.PSVersion -lt [version]'5.1') {
-    $nugetBinPath = "$env:LOCALAPPDATA\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
-    "Checking for NuGet.exe" | Write-Host
+    $nugetBinPath = "$env:LOCALAPPDATA\Microsoft\Windows\PowerShell\PowerShellGet\nuget.exe"
+    "Checking for nuget.exe" | Write-Host
     if (!(Test-Path -Path $nugetBinPath -PathType Leaf)) {
         New-Item -Path (Split-Path -Path $nugetBinPath -Parent) -ItemType Directory -Force > $null
-        "Downloading NuGet.exe" | Write-Host
+        "Downloading nuget.exe" | Write-Host
         Invoke-WebRequest -Uri 'https://dist.nuget.org/win-x86-commandline/latest/nuget.exe' -OutFile $nugetBinPath
     }
     Get-ChildItem -Path (Split-Path -Path $nugetBinPath -Parent)
     & $nugetBinPath
 }else {
-    "Installation is not needed." | Write-Host
+    "Installation is not required." | Write-Host
 }
