e7dae9cd72457884b15286a5c2fd3aebe41cb317
Joel Timothy Oh <joeltimothyoh@live.com>
2023-09-08 07:40:35 +0000

    Enhancement: Show warning in `Invoke-Publish.ps1` if `dotnet` cli is not found
    Co-authored-by: leo <leonardjonathanoh@live.com>

--- src/PSModulePublisher/Public/Invoke-Publish.ps1
+++ src/PSModulePublisher/Public/Invoke-Publish.ps1
@@ -18,6 +18,16 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 
 try {
+    "Check publish dependencies" | Write-Host
+    if (!(Get-Command -Name dotnet -CommandType Application -ErrorAction SilentlyContinue)) {
+        @"
+dotnet is required for Publish-Module, but is not installed. To install:
+  Alpine: apk add icu-data-full --repository=https://dl-cdn.alpinelinux.org/alpine/v3.16/main && apk add lttng-ust --repository=https://dl-cdn.alpinelinux.org/alpine/v3.16/main && apk add dotnet6-runtime --repository=https://dl-cdn.alpinelinux.org/alpine/v3.16/community
+  Ubuntu: sudo apt-get update && sudo apt-get install -y dotnet-runtime-6.0
+  Windows: https://aka.ms/dotnet-install-script. See script usage: https://github.com/dotnet/docs/blob/main/docs/core/tools/dotnet-install-script.md
+"@ | Write-Warning
+    }
+
     "Publish the module" | Write-Host
     & "$PSScriptRoot\..\Private\Publish-Module.ps1" -Path $ModuleManifestPath -Repository $Repository -DryRun:$DryRun
 
