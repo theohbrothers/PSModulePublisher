f80f8603372ad2fd0b8bc439c129db8b19659bb5
Joel Timothy Oh <joeltimothyoh@live.com>
2024-05-27 17:09:05 +0000

    Test (integration): Update test module script module file

--- test/Mock-Module/src/Mock-Module/Mock-Module.psm1
+++ test/Mock-Module/src/Mock-Module/Mock-Module.psm1
@@ -1,16 +1,4 @@
-$script:MODULE_BASE_DIR = Split-Path $MyInvocation.MyCommand.Path -Parent
-# $script:MODULE_HELPER_DIR = Join-Path $MODULE_BASE_DIR 'Helper'
-# $script:MODULE_PRIVATE_DIR = Join-Path $MODULE_BASE_DIR 'Private'
-# $script:MODULE_PUBLIC_DIR = Join-Path $MODULE_BASE_DIR 'Public'
+Set-StrictMode -Version Latest
 
-# Get-ChildItem "$script:MODULE_HELPER_DIR/*.ps1" -Exclude *.Tests.ps1 | % {
-#     . $_.FullName
-# }
-
-# Get-ChildItem "$script:MODULE_PRIVATE_DIR/*.ps1" -Exclude *.Tests.ps1 | % {
-#     . $_.FullName
-# }
-
-# Get-ChildItem "$script:MODULE_PUBLIC_DIR/*.ps1" -Exclude *.Tests.ps1 | % {
-#     . $_.FullName
-# }
+# Initialize variables
+$MODULE_BASE_DIR = $PSScriptRoot
