2d0b9abda7493b1d4bff1fffefcca24d0b87d3c8
Joel Timothy Oh <joeltimothyoh@live.com>
2024-06-01 00:43:41 +0000

    Test (integration): Add additional tests for cmdlets `Invoke-Publish` and `Invoke-PSModulePublisher`

--- src/PSModulePublisher/PSModulePublisher.Tests.ps1
+++ src/PSModulePublisher/PSModulePublisher.Tests.ps1
@@ -12,20 +12,40 @@ Describe "PSModulePublisher" -Tag 'Integration' {
         Pop-Location
     }
     It "Runs Invoke-Build" {
+        $env:MODULE_VERSION = $null
         $moduleManifest = Invoke-Build
         $moduleManifest | Should -Be $mockModuleManifest
     }
     It "Runs Invoke-Test" {
         Invoke-Test -ModuleManifestPath $mockModuleManifest
     }
-    It "Runs Invoke-Publish" {
-        $env:MODULE_VERSION = '0.0.0'
+    It "Runs Invoke-Publish -DryRun `$env:NUGET_API_KEY" {
         $env:NUGET_API_KEY = 'xxx'
         Invoke-Publish -ModuleManifestPath $mockModuleManifest -Repository PSGallery -DryRun
     }
-    It "Runs Invoke-PSModulePublisher" {
-        $env:MODULE_VERSION = '0.0.0'
+    It "Runs Invoke-Build `$env:MODULE_VERSION='0.1.0'" {
+        $env:MODULE_VERSION = '0.1.0'
+        $moduleManifest = Invoke-Build
+        $moduleManifest | Should -Be $mockModuleManifest
+    }
+    It "Runs Invoke-Publish -Repository `$env:MODULE_VERSION='0.1.0'" {
+        $env:MODULE_VERSION = '0.1.0'
+        $env:NUGET_API_KEY = $null
+        { Invoke-Publish -ModuleManifestPath $mockModuleManifest -Repository PSGallery } | Should -Throw
+    }
+    It "Runs Invoke-PSModulePublisher -Repository -DryRun `$env:NUGET_API_KEY" {
+        $env:MODULE_VERSION = $null
         $env:NUGET_API_KEY = 'xxx'
         Invoke-PSModulePublisher -Repository PSGallery -DryRun
+    }
+    It "Runs Invoke-PSModulePublisher -Repository `$env:NUGET_API_KEY" {
+        $env:MODULE_VERSION = $null
+        $env:NUGET_API_KEY = 'xxx'
+        { Invoke-PSModulePublisher -Repository PSGallery } | Should -Throw
+    }
+    It "Runs Invoke-PSModulePublisher -Repository `$env:MODULE_VERSION='0.1.0'" {
+        $env:MODULE_VERSION = '0.1.0'
+        $env:NUGET_API_KEY = $null
+        { Invoke-PSModulePublisher -Repository PSGallery } | Should -Throw
     }
 }
